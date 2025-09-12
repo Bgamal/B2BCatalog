f9f04d1243879fe9506961cb1fd7364a61948632
Bassent Mahmoud <Bassent.Mahmoud@linkdev.com>
2025-09-12 13:45:10 +0300

    adding theme to the project-next

---
+++ strapi-b2b/config/database--.js
@@ -0,0 +1,12 @@
+import type { DatabaseConfig } from '@strapi/strapi';
+
+export default ({ env }): DatabaseConfig => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+    debug: false,
+  },
+});
